9b3b9c43d6ac988de17b8c53cf18b6f58f66be34
Kit Pyles <pyles.kit@gmail.com>
2025-02-13 21:52:51 -0600

    Build: Initial commit of backend codebase imported from start.spring.io

---
+++ src/main/java/com/kitpyles/ravensnestegg/RavensnesteggApplication.java
@@ -0,0 +1,13 @@
+package com.kitpyles.ravensnestegg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavensnesteggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavensnesteggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kitpyles/ravensnestegg/RavensnesteggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitpyles.ravensnestegg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavensnesteggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
