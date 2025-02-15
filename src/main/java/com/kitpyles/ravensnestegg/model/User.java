2d7004f6007e4eeeb1273dc65fe78f9bcbc9009e
Kit Pyles <pyles.kit@gmail.com>
2025-02-14 20:33:43 -0600

    Fix: Debug issues with Lombok & list error

--- src/main/java/com/kitpyles/ravensnestegg/model/User.java
+++ src/main/java/com/kitpyles/ravensnestegg/model/User.java
@@ -1,13 +1,13 @@
 package com.kitpyles.ravensnestegg.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.Collections;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
@@ -40,7 +40,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(new SimpleGrantedAuthority(role));
+        return Collections.singleton(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
