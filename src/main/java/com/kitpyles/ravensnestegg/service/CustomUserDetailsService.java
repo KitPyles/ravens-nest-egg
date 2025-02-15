54d78369aa962186d25a997abbc8161357c725b9
Kit Pyles <pyles.kit@gmail.com>
2025-02-15 14:55:35 -0600

    Build: Create repository & details service to manage users

---
+++ src/main/java/com/kitpyles/ravensnestegg/service/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.kitpyles.ravensnestegg.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.kitpyles.ravensnestegg.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
