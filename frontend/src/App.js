5882b8a040f82e8ef08338fcc8e83ca885c278ce
Kit Pyles <pyles.kit@gmail.com>
2025-03-01 17:55:27 -0600

    Fix: Troubleshooting issues with Nav rendering.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Nav from './components/Nav';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
-      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/dashboard" element={<Dashboard />} />
