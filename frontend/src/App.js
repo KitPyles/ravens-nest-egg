d97cb6d1ba8943e31f8d9333f1ee0b8e30c2badc
Kit Pyles <pyles.kit@gmail.com>
2025-03-02 17:25:30 -0600

    Build: Preliminary Dashboard page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/expenses" element={<Expenses />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/expenses" element={<Expenses />} />
         <Route path="/income" element={<Income />} />
         <Route path="/settings" element={<Settings />} /> */}
       </Routes>
