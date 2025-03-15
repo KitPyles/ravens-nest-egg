842be52aea446b7de8637295f7636acd01e2320b
Kit Pyles <pyles.kit@gmail.com>
2025-03-15 17:51:41 -0500

    Fix: Trying to fix all the ways that Spring Security broke this thing. (Unsuccessful so far)

--- src/main/java/com/kitpyles/ravensnestegg/model/User.java
+++ src/main/java/com/kitpyles/ravensnestegg/model/User.java
@@ -1,11 +1,6 @@
 package com.kitpyles.ravensnestegg.model;
 
 import jakarta.persistence.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import java.util.Collection;
-import java.util.Collections;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Table(name = "users")
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -36,31 +31,6 @@ public class User implements UserDetails {
 
     public enum Role {
         USER, ADMIN
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority(role.name()));
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
     }
 
 }    
